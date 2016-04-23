dad6cb9093b19eee83ba034de5b43723c26fe740
PreFX48 <kerrovitarr@gmail.com>
2016-04-23 14:44:14 +0300

    Добавлен дополнительный признак - количество слов в комментарии

--- text_analysis.py
+++ text_analysis.py
@@ -2,8 +2,11 @@ import json
 from text_preparation import prepareText
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.preprocessing import MaxAbsScaler
 from sklearn.naive_bayes import MultinomialNB
+import sklearn.preprocessing
 import numpy
+import scipy.sparse
 numpy.set_printoptions(threshold=numpy.nan)
 
 
@@ -30,30 +33,36 @@ def initClassifierEnvironment(jsonFilePath):
             bdates.append(person['bdate'])
     comments = list(map(prepareText, comments))
     editedTexts = list(map(" ".join, comments))
+    lengths = list(map(len, comments))
+    lengthsArray = numpy.empty((len(lengths), 1))
+    for i in range(0, len(lengths)):
+        lengthsArray[i][0] = lengths[i]
     vectorizer = CountVectorizer(ngram_range=(1, 2))
     wordCounts = vectorizer.fit_transform(editedTexts).toarray()
     transformer = TfidfTransformer()
     weightedCounts = transformer.fit_transform(wordCounts).toarray()
-
-
+    finalFeatureMatrix = numpy.concatenate((weightedCounts, lengthsArray), axis=1)
+    scaler = MaxAbsScaler()
+    finalFeatureMatrix = scaler.fit_transform(finalFeatureMatrix)
     classifier = MultinomialNB()
-    classifier.fit(weightedCounts, bdates)
+    classifier.fit(finalFeatureMatrix, bdates)
 
 
     mistake = 0
     number = 0
     for comment, bdate in zip(comments, bdates):
-        mistake += abs(bdate - classifier.predict(transformer.transform(vectorizer.transform([' '.join(comment)]))))
+        features = transformer.transform(vectorizer.transform([' '.join(comment)]))
+        length = numpy.empty((1, 1))
+        length[0][0] = len(comment)
+        length = scipy.sparse.csr_matrix(length)
+        features = scipy.sparse.hstack((features, length))
+        features = scaler.transform(features)
+        mistake += abs(bdate - classifier.predict(features))
         number += 1
     print('Min year:', minYear)
     print('Max year:', maxYear)
     print('Average year:', totalYear / peopleNumber)
     print('Average mistake:', mistake / number)
 
-    return (vectorizer, transformer, classifier)
 
-environment = initClassifierEnvironment('data.json')
-vectorizer = environment[0]
-tfidfTransformer = environment[1]
-classifier = environment[2]
-
+initClassifierEnvironment('data.json')
