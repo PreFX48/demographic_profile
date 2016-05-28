af97584255d859e828c37ec048315956d02fcd54
PreFX48 <kerrovitarr@gmail.com>
2016-05-28 18:34:25 +0300

    Теперь комментарии классифицируются по нескольким возрастным категориям. Добавлена логистическая регрессия. Имена переменных изменены на python-way.

--- text_analysis.py
+++ text_analysis.py
@@ -4,65 +4,92 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.preprocessing import MaxAbsScaler
 from sklearn.naive_bayes import MultinomialNB
-import sklearn.preprocessing
+from sklearn.linear_model import LogisticRegression
 import numpy
 import scipy.sparse
-numpy.set_printoptions(threshold=numpy.nan)
 
 
-def initClassifierEnvironment(jsonFilePath):
-    jsonData = []
-    comments = []
+def getAgeCategory(birth_year):
+    CURRENT_YEAR = 2016
+    age = CURRENT_YEAR - birth_year
+    if 0 < age <= 18:
+        age_category = '0-18'
+    elif 19 <= age <= 25:
+        age_category = '19-25'
+    elif 26 <= age <= 35:
+        age_category = '26-35'
+    elif 36 <= age <= 45:
+        age_category = '36-45'
+    else:
+        age_category = '46+'
+    return age_category
+
+
+def initClassifierEnvironment(json_file_path):
+    json_data = []
+    raw_comments = []
     bdates = []
-    with open(jsonFilePath, 'r') as fp:
-        jsonData = json.load(fp)
-    minYear = 2016
-    maxYear = 1
-    totalYear = 0
-    peopleNumber = 0
-    for person in jsonData:
-        if peopleNumber >= 1000:
+    with open(json_file_path, 'r') as fp:
+        json_data = json.load(fp)
+    min_year = 2016
+    max_year = 1
+    total_year = 0
+    people_number = 0
+    for person in json_data:
+        if people_number >= 1000:
             break
         if 1950 <= person['bdate'] <= 2010:
-            minYear = min(minYear, person['bdate'])
-            maxYear = max(maxYear, person['bdate'])
-            totalYear += person['bdate']
-            peopleNumber += 1
+            min_year = min(min_year, person['bdate'])
+            max_year = max(max_year, person['bdate'])
+            total_year += person['bdate']
+            people_number += 1
         for comment in person['user_comments']:
-            comments.append(comment)
+            raw_comments.append(comment)
             bdates.append(person['bdate'])
-    comments = list(map(prepareText, comments))
-    editedTexts = list(map(" ".join, comments))
-    lengths = list(map(len, comments))
-    lengthsArray = numpy.empty((len(lengths), 1))
-    for i in range(0, len(lengths)):
-        lengthsArray[i][0] = lengths[i]
+    age_categories = list(map(getAgeCategory, bdates))
+    comments = list(map(prepareText, raw_comments))
+    word_numbers = list(map(len, comments))
+    word_numbers_array = numpy.empty((len(word_numbers), 1))
+    for i in enumerate(word_numbers):
+        word_numbers_array[i[0]][0] = i[1]
+    word_numbers_array = scipy.sparse.csr_matrix(word_numbers_array)
+    edited_texts = list(map(" ".join, comments))
     vectorizer = CountVectorizer(ngram_range=(1, 2))
-    wordCounts = vectorizer.fit_transform(editedTexts).toarray()
+    word_counts = vectorizer.fit_transform(edited_texts)
     transformer = TfidfTransformer()
-    weightedCounts = transformer.fit_transform(wordCounts).toarray()
-    finalFeatureMatrix = numpy.concatenate((weightedCounts, lengthsArray), axis=1)
+    weighted_counts = transformer.fit_transform(word_counts)
+    final_feature_matrix = scipy.sparse.hstack([weighted_counts, word_numbers_array])
     scaler = MaxAbsScaler()
-    finalFeatureMatrix = scaler.fit_transform(finalFeatureMatrix)
-    classifier = MultinomialNB()
-    classifier.fit(finalFeatureMatrix, bdates)
+    final_feature_matrix = scaler.fit_transform(final_feature_matrix)
+    NB_classifier = MultinomialNB()
+    NB_classifier.fit(final_feature_matrix, age_categories)
+    logistic_classifier = LogisticRegression()
+    logistic_classifier.fit(final_feature_matrix, age_categories)
 
 
-    mistake = 0
+    bayes_mistake = 0
+    logistic_mistake = 0
     number = 0
-    for comment, bdate in zip(comments, bdates):
+    for raw_comment, comment, age_category in zip(raw_comments, comments, age_categories):
         features = transformer.transform(vectorizer.transform([' '.join(comment)]))
-        length = numpy.empty((1, 1))
-        length[0][0] = len(comment)
-        length = scipy.sparse.csr_matrix(length)
-        features = scipy.sparse.hstack((features, length))
+        words_number = numpy.empty((1, 1))
+        words_number[0][0] = len(comment)
+        words_number = scipy.sparse.csr_matrix(words_number)
+        features = scipy.sparse.hstack((features, words_number))
         features = scaler.transform(features)
-        mistake += abs(bdate - classifier.predict(features))
+        if age_category != NB_classifier.predict(features):
+            bayes_mistake += 1
         number += 1
-    print('Min year:', minYear)
-    print('Max year:', maxYear)
-    print('Average year:', totalYear / peopleNumber)
-    print('Average mistake:', mistake / number)
+        if age_category != logistic_classifier.predict(features):
+            logistic_mistake += 1
+    print('Min year:', min_year)
+    print('Max year:', max_year)
+    print('Average year:', total_year / people_number)
+    print('Naive Bayes correct in:', str(round(100 - bayes_mistake*100 / number, 1)) + '%')
+    print('Logistic regression correct in:', str(round(100 - logistic_mistake*100 / number, 1)) + '%')
 
-
+import timeit
+start = timeit.default_timer()
 initClassifierEnvironment('data.json')
+stop = timeit.default_timer()
+print("TIME:", stop - start)
